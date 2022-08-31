a66200b33c311ac964140d408a48179bbdb7c0d5
zhuyahui <zhuyahui@yovole.com>
2022-09-01 00:08:47 +0800

    0901

---
+++ src/main/java/cn/Main.java
@@ -0,0 +1,9 @@
+package cn;
+
+import com.webserver.core.WebServerApplication;
+
+public class Main {
+    public static void main(String[] args) {
+        WebServerApplication.run(Main.class,args);
+    }
+}
